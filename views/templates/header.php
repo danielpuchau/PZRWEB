f2afc3581c1b167a5fd266cfaa92d38544f42204
daniepuchau <daniel.puchau@gmail.com>
2023-03-03 01:41:04 +0100

    cambio Logo

--- views/templates/header.php
+++ views/templates/header.php
@@ -10,7 +10,7 @@
 
             <div class="header__contenido">  <!-- logo-container -->
                 <div class="header__logo">  <!-- logo -->
-                    <img src="/build/img/logo1.jpg" alt="icono menu responsive" width="100">
+                    <img src="/build/img/LOGO1.jpg" alt="icono menu responsive" width="100">
                 </div>
                 <h1 class="header__titulo">PRINZEDOM RECORDS</h1>
             </div>
